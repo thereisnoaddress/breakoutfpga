82fddc58622ea55fcd6f6dac6aef53132e8f0535
Chris <chrie.zhu@mail.utoronto.ca>
2017-11-19 20:12:42 -0500

    updated

--- collision_check.v
+++ collision_check.v
@@ -62,8 +62,8 @@ endmodule
 // Check if X0 + xstep >= X1 or Y0 + ystep >= Y1
 module collision_check(X0, Y0, X1, Y1, xstep, ystep, collision, clk);
 	input clk;
-	input [7:0] X0, X1; 
-	input [6:0] Y0, Y1;
+	input [9:0] X0, X1; 
+	input [9:0] Y0, Y1;
 	input [6:0] xstep, ystep;
 	input clk;
 	output reg [1:0] collision; // MSB 0 if no collision, 1 if collision
@@ -91,25 +91,28 @@ module change_direction_collision(collision_code, original_dir, new_dir);
 
 
 	always @(*) begin
-		if (collision_code[1] == 0) begin // X collision
-			case(original_dir)
-				2'b00: new_dir = 2'b01;
-				2'b01: new_dir = 2'b00;
-				2'b10: new_dir = 2'b11;
-				2'b11: new_dir = 2'b10;
-				default: new_dir = original_dir;
-			endcase // original_dir
-		end
-		else if (collision_code[1] == 1) begin // Y collision
-			case (original_dir)
-				2'b00: new_dir = 2'b10; 
-				2'b01: new_dir = 2'b11;
-				2'b10: new_dir = 2'b00;
-				2'b11: new_dir = 2'b01;
-				default : new_dir = original_dir;
-			endcase
-		end
-	end
+	   if (collision_code[1] == 1) begin
+	      if (collision_code[0] == 0) begin // X collision
+		 case(original_dir)
+		   2'b00: new_dir = 2'b01;
+		   2'b01: new_dir = 2'b00;
+		   2'b10: new_dir = 2'b11;
+		   2'b11: new_dir = 2'b10;
+		   default: new_dir = original_dir;
+		 endcase // original_dir
+	      end
+	      else if (collision_code[0] == 1) begin // Y collision
+		 case (original_dir)
+		   2'b00: new_dir = 2'b10; 
+		   2'b01: new_dir = 2'b11;
+		   2'b10: new_dir = 2'b00;
+		   2'b11: new_dir = 2'b01;
+		   default : new_dir = original_dir;
+		 endcase
+	      end
+	   end // if (collision_code[0] == 1)
+	end // always @ begin
+   
 
 
 endmodule // module
--- teststuff.do
+++ teststuff.do
@@ -1,23 +1,43 @@
 vlib work
 
 vlog -timescale 1ns/1ns collision_check.v
-vsim update_ball
+vsim change_direction_collision
 
 log {/*}
 add wave {/*}
 
-#testing update_ball
-force { X[9:0]} 10'b0000100010
-force { Y[9:0]} 10'b0001000100
-force clk 0 0, 1 5 -r 10
-force {dir[1:0]} 2'b 00
-run 40ns
+#testing change_direction_collision - OK
+#force {collision_code [1:0]} 2'b00
+#force {original_dir[1:0]} 2'b00
+#run 60ns
+
+
+
+#testing collision_check - OK
+#force clk 0 0, 1 5 -r 10
+#force {X0[9:0]} 9'b000000100
+#force {Y0[9:0]} 9'b000100000
+#force {X1[9:0]} 9'b000000110
+#force {Y1[9:0]} 9'b000000100
+#force {xstep[6:0]} 2'b10
+#force {ystep[6:0]} 2'b01
+#run 60ns
+
 
 
 
     
 
-#Testing paddle_shift
+#testing update_ball - OK
+#force { X[9:0]} 10'b0000100010
+#force { Y[9:0]} 10'b0001000100
+#force clk 0 0, 1 5 -r 10
+#force {dir[1:0]} 2'b 00
+#run 40ns
+
+   
+
+#Testing paddle_shift - OK
 #force clk 0 0, 1 5 -r 10
 #force {KEY[2]} 1
 #run 30ns
