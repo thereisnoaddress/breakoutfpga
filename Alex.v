42aa0e48ebeaa6cdea91b7527fcec855654ef721
Chris <chrise.zhu@mail.utoronto.ca>
2017-12-04 17:46:48 -0500

    new

--- Alex.v
+++ Alex.v
@@ -15,7 +15,9 @@ module Alex
 		VGA_R,   						//	VGA Red[9:0]
 		VGA_G,	 						//	VGA Green[9:0]
 		VGA_B,   						//	VGA Blue[9:0]
-		LEDR
+		LEDR, 
+		HEX0,
+		HEX1
 	);
 
 	input			CLOCK_50;				//	50 MHz
@@ -32,7 +34,9 @@ module Alex
 	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
 	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
 	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
-	output [7:0]LEDR;
+	output 	[7:0]	LEDR;
+	output 	[6:0] 	HEX0;
+	output 	[6:0] 	HEX1;
 	wire resetn;
 	assign resetn = KEY[2];
 	
@@ -187,8 +191,44 @@ module Alex
 	 .currrent(current)
 	 );
 	 
+	 hex_decoder h0(
+	 				.hex_digit(current[3:0]), 
+	 				.segments(HEX0)
+	 				);
+
+	 hex_decoder h1(
+	 				.hex_digit(current[7:4]), 
+	 				.segments(HEX1)
+	 				);
 
 
     
+endmodule
+
+
+module hex_decoder(hex_digit, segments);
+    input [3:0] hex_digit;
+    output reg [6:0] segments;
+   
+    always @(*)
+        case (hex_digit)
+            4'h0: segments = 7'b100_0000;
+            4'h1: segments = 7'b111_1001;
+            4'h2: segments = 7'b010_0100;
+            4'h3: segments = 7'b011_0000;
+            4'h4: segments = 7'b001_1001;
+            4'h5: segments = 7'b001_0010;
+            4'h6: segments = 7'b000_0010;
+            4'h7: segments = 7'b111_1000;
+            4'h8: segments = 7'b000_0000;
+            4'h9: segments = 7'b001_1000;
+            4'hA: segments = 7'b000_1000;
+            4'hB: segments = 7'b000_0011;
+            4'hC: segments = 7'b100_0110;
+            4'hD: segments = 7'b010_0001;
+            4'hE: segments = 7'b000_0110;
+            4'hF: segments = 7'b000_1110;   
+            default: segments = 7'h7f;
+        endcase
 endmodule
 
